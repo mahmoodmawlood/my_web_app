8c973318bc69e23edcd737ae094438ae9138c577
Mahmood Mawlood <mahmoodmawlood@gmail.com>
2025-11-10 18:26:51 +0300

    Deploy fixed service worker

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "efc2dd2c3236c54c9f6fd2fa4a6d465e",
+"flutter_bootstrap.js": "aed114ddc25760e55f7e2388cdd42ef3",
 "icons/Icon-192.png": "d54f40981fd6dee81e31f610c10585e3",
 "icons/Icon-512.png": "d54f40981fd6dee81e31f610c10585e3",
 "index.html": "8e322bac40bfde25a702d30890291bd7",
